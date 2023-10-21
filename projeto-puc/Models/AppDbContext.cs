eabea1daa8f6649f44c02f32307cae0c3a290827
Leonardo Alves <leonardo.freitas.1429449@sga.ucminas.br>
2023-10-21 15:56:14 -0300

    Configuração Banco de Dados e Tabela Veículos

---
+++ projeto-puc/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace projeto_puc.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
